94e9085746d2ab861d834d0192de2b98c28853d8
ehabalshrida <ehabshridah@gmail.com>
2021-09-07 14:08:48 +0300

    added img

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -20,7 +20,7 @@ import React, { Component } from 'react'
                 <h2>{this.props.title}</h2>
                 <img src={this.props.image_url} alt="" onClick={this.clickImage}></img>
                 <p>{this.props.description}</p>
-                <p>Clicks:{this.state.clicks}</p>
+                <div>Clicks:{this.state.clicks}<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJVGFHOMTZJI2seOBPw0ZeEWBjKGTPSabMvQ&usqp=CAU"height="40px"width="40px"/></div>
             </>
         )
     }
